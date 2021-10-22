992a72dd70c896b88ee2f97a8f70892a46f80b37
gvkhirwadkar <gvkhirwadkar@gmail.com>
2021-10-22 19:38:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function someFunction() {
+  for (var i = 0; i < 10; i++) {
+    var k = i;
+  }
+  if (i) {
+    console.log('I is here', i);
+  } else {
+    console.log('K is here', k);
+  }
+}
+
+someFunction();
+
+var arg = 100;
+console.log('Outside', arg);
+
+function someOtherFunc() {
+  let arg = 10;
+  console.log('Inside let', arg);
+}
+
+someOtherFunc(1);
+console.log('Outside', arg);
